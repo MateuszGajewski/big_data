cce9d7b829267d218cdb2db66dc2a81761236469
mackowiakw <58404185+mackowiakw@users.noreply.github.com>
2022-01-14 21:53:02 +0100

    Update create.sh

--- hurtownia/create.sh
+++ hurtownia/create.sh
@@ -8,5 +8,5 @@ spark-submit --master yarn --num-executors 5 --driver-memory 512m --executor-mem
 on" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" przestrzenETL.jar project
 spark-submit --master yarn --num-executors 5 --driver-memory 512m --executor-memory 512m --executor-cores 1 --packages io.delta:delta-core_2.12:0.8.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtensi
 on" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" typPojazduETL.jar project
-spark-submit --master yarn --num-executors 5 --driver-memory 512m --executor-memory 512m --executor-cores 1 --packages io.delta:delta-core_2.12:0.8.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtensi
-on" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" faktyETL.jar project
+# spark-submit --master yarn --num-executors 5 --driver-memory 512m --executor-memory 512m --executor-cores 1 --packages io.delta:delta-core_2.12:0.8.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtensi
+# on" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" faktyETL.jar project
