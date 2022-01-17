b3d69d0969946db8b2116f5cacd0f0107cee07b1
MateuszGajewski <mg96272@gmail.com>
2022-01-17 19:19:17 +0100

    Add drp_table

--- hurtownia/create.sh
+++ hurtownia/create.sh
@@ -1,3 +1,4 @@
+cat drop_tables.scala | spark-shell --packages io.delta:delta-core_2.12:0.8.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" -i
 spark-submit --master yarn --num-executors 5 --driver-memory 512m --executor-memory 512m --executor-cores 1 --packages io.delta:delta-core_2.12:0.8.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtensi
 on" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" czasETL.jar project
 spark-submit --master yarn --num-executors 5 --driver-memory 512m --executor-memory 512m --executor-cores 1 --packages io.delta:delta-core_2.12:0.8.0 --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtensi
